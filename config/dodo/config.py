e75e213ae7d00b78d7420abe2c99746ca7964ad3
Karel Kočí <cynerd@email.cz>
2025-05-09 15:07:55 +0200

    dodo: tweak some configuration

--- config/dodo/config.py
+++ config/dodo/config.py
@@ -14,8 +14,9 @@ dodo.settings.sent_dir = {
 dodo.settings.gnupg_keyid = "2B1F70F95F1B48DA2265A7FAA6BC8B8CEB31659B"
 
 dodo.settings.theme = dodo.themes.nord
+dodo.settings.wrap_message = False
 dodo.settings.editor_command = "alacritty -e nvim '{file}'"
-dodo.settings.file_browser_command = "alacritty -e ranger '{dir}'"
+dodo.settings.file_browser_command = "alacritty -e mc '{dir}'"
 dodo.settings.file_picker_command = "alacritty -e ranger --choosefiles='{tempfile}'"
 dodo.settings.sync_mail_interval = -1
 dodo.settings.default_thread_list_mode = "thread"
@@ -35,6 +36,7 @@ dodo.settings.tag_icons = {
 
 dodo.settings.default_to_html = False
 dodo.settings.html_block_remote_requests = False
+dodo.settings.html_confirm_open_links = False
 dodo.util.html2html = dodo.util.clean_html2html
 
 del dodo.keymap.global_keymap["`"]
