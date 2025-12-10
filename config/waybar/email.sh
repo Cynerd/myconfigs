71170ba96ad788726af419edd64d58bdfe5b52b5
Karel Kočí <cynerd@email.cz>
2025-12-10 12:19:17 +0100

    Format and add guix install

--- config/waybar/email.sh
+++ config/waybar/email.sh
@@ -10,7 +10,7 @@ fi
 part=()
 for mail in email gmail elektroline fel; do
 	counts="$(notmuch count -- "${filter[@]}" and "tag:$mail")"
-	[ "$counts" = "0" ] || \
+	[ "$counts" = "0" ] ||
 		part+=("$mail:$counts")
 done
 
